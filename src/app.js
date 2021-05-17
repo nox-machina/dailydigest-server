d53db44057af561bdaca5038e6241233c0125b65
0xtheMachine <aaronvsummers@gmail.com>
2021-05-17 20:19:14 +0100

    env var fix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 
 //Database
-mongoose.connect(`${config.DB_URL}`, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}, () => {
+mongoose.connect(`${config.DB_URL || process.env.DB_URL}`, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}, () => {
  console.log("connected")
 })
 
